59434eedb159d1098bad02382b97a8719a9b88a5
Gabriel Kania <gracz1212yt@gmail.com>
2026-04-29 16:30:28 +0200

    Initial project setup

---
+++ src/main/java/gorbiel/stock_sim/StockSimApplication.java
@@ -0,0 +1,13 @@
+package gorbiel.stock_sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gorbiel/stock_sim/StockSimApplicationTests.java
@@ -0,0 +1,13 @@
+package gorbiel.stock_sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
