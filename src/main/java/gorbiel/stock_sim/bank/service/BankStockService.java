514a18edfd9a04efa82da44270bea2dc83860bbb
Gabriel Kania <gracz1212yt@gmail.com>
2026-05-03 01:19:50 +0200

    feat: add GET endpoint for Bank Stocks

--- src/main/java/gorbiel/stock_sim/bank/service/BankStockService.java
+++ src/main/java/gorbiel/stock_sim/bank/service/BankStockService.java
@@ -1,8 +1,11 @@
 package gorbiel.stock_sim.bank.service;
 
+import gorbiel.stock_sim.bank.dto.BankStocksResponse;
 import gorbiel.stock_sim.bank.dto.UpdateBankStocksRequest;
 
 public interface BankStockService {
 
     void updateBankStocks(UpdateBankStocksRequest request);
+
+    BankStocksResponse getBankStocks();
 }
