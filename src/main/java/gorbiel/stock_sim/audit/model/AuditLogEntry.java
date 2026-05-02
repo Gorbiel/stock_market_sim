e45931c74df2c4aacb1f31db17f4727325af05c1
Gabriel Kania <gracz1212yt@gmail.com>
2026-05-02 15:18:06 +0200

    feat: add JPA annotations

--- src/main/java/gorbiel/stock_sim/audit/model/AuditLogEntry.java
+++ src/main/java/gorbiel/stock_sim/audit/model/AuditLogEntry.java
@@ -1,31 +1,37 @@
 package gorbiel.stock_sim.audit.model;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-// @Entity
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AuditLogEntry {
 
-    //    @Id
-    //    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    //    @Enumerated(EnumType.STRING)
-    //    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private OperationType type;
 
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private String walletId;
 
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private String stockName;
 
-    //    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Instant createdAt;
 
     public AuditLogEntry(OperationType type, String walletId, String stockName) {
