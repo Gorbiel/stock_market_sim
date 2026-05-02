15a318e78ecc71603c2c424c4c7deb410f31514a
Gabriel Kania <gracz1212yt@gmail.com>
2026-05-03 01:09:47 +0200

    feat: add bank stock service layer

---
+++ src/main/java/gorbiel/stock_sim/bank/service/BankStockService.java
@@ -0,0 +1,8 @@
+package gorbiel.stock_sim.bank.service;
+
+import gorbiel.stock_sim.bank.dto.UpdateBankStocksRequest;
+
+public interface BankStockService {
+
+    void updateBankStocks(UpdateBankStocksRequest request);
+}
