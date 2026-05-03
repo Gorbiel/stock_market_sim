0ebe9690fe1a6a4b68d7e859c572e4708c937cb5
Gabriel Kania <gracz1212yt@gmail.com>
2026-05-03 21:03:59 +0200

    fix: use NotEmpty instead of NotNull in UpdateBankStocksRequest

--- src/main/java/gorbiel/stock_sim/bank/dto/UpdateBankStocksRequest.java
+++ src/main/java/gorbiel/stock_sim/bank/dto/UpdateBankStocksRequest.java
@@ -1,7 +1,7 @@
 package gorbiel.stock_sim.bank.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
-public record UpdateBankStocksRequest(@NotNull List<@Valid BankStockItemRequest> stocks) {}
+public record UpdateBankStocksRequest(@NotEmpty List<@Valid BankStockItemRequest> stocks) {}
